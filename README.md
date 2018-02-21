5e3be6581397c2dc6b053068f7393ce94efab47f
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-02-21 18:12:42 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # karma-jasmine-extra-matchers
-A Karma plugin to inject jasmine-extra-matchers
+
+[![Code Climate](https://codeclimate.com/github/tomi77/karma-jasmine-extra-matchers/badges/gpa.svg)](https://codeclimate.com/github/tomi77/karma-jasmine-extra-matchers)
+[![dependencies Status](https://david-dm.org/tomi77/karma-jasmine-extra-matchers/status.svg)](https://david-dm.org/tomi77/karma-jasmine-extra-matchers)
+![Downloads](https://img.shields.io/npm/dt/karma-jasmine-extra-matchers.svg)
+
+A [Karma](https://karma-runner.github.io/2.0/) plugin to inject [jasmine-extra-matchers](https://github.com/tomi77/jasmine-extra-matchers)
+
+## Installation
+
+~~~bash
+npm install karma-jasmine-extra-matchers --save-dev
+~~~
+
+## Usage
+
+Include ``jasmine-extra-matchers`` in the frameworks and ``karma-jasmine-extra-matchers`` in the plugins section of your config
+
+~~~js
+module.exports = function(config) {
+  config.set({
+    frameworks: [
+      'jasmine',
+      'jasmine-extra-matchers'
+    ],
+    files: [
+      'src/**/*.js',
+      'src/**/*.spec.js'
+    ],
+    // also you must add it as a plugin
+    plugins: [
+      'karma-jasmine',
+      'karma-jasmine-extra-matchers'
+    ]
+  });
+};
+~~~
---
+++ index.js
@@ -0,0 +1,80 @@
+(function() {
+
+  'use strict';
+
+  function JasmineExtraMatchers(files) {
+
+    /**
+     * @description
+     * Add a new array item after the one at the supplied index.
+     *
+     * @param  {Array}  array
+     * @param  {Number} index
+     * @param  {*}      item
+     */
+    function insertAfter(array, index, item) {
+      array.splice(index + 1, 0, item);
+    }
+
+    /**
+     * @description
+     * Is this file configuation the the "karma-jasmine" framework's jasmine library?
+     *
+     * @param  {Object}  file
+     * @return {Boolean}
+     */
+    function isJasmine(file) {
+      return file.pattern.search(/karma\-jasmine(\/|\\)lib(\/|\\)adapter\.js/) !== -1;
+    }
+
+    /**
+     * @description
+     * Get the array index of the "karma-jasmine" framework plugin in the files configuration.
+     *
+     * @param  {Object[]} files
+     * @return {Number}
+     */
+    function indexOfJasmine(files) {
+      for (var i = 0, len = files.length; i < len; i++) {
+        if (isJasmine(files[i])) {
+          return i;
+        }
+      }
+      return -1;
+    }
+
+    /**
+     * @description
+     * Locate the Jasmine Extra Matchers library.
+     *
+     * @return {String}
+     */
+    function getLibPath() {
+      return require.resolve('jasmine-extra-matchers').replace('index.js', 'dist/jasmine-extra-matchers.js');
+    }
+
+    // Init
+    // ---------------------------------------------------------------------------------------------
+
+    var idx = indexOfJasmine(files);
+
+    if (idx !== -1) {
+      insertAfter(files, idx, {
+        pattern: getLibPath(),
+        included: true,
+        served: true,
+        watched: false
+      });
+    } else {
+      throw new Error('"jasmine" needs to appear before "jasmine-extra-matchers" in the "frameworks" array of your Karma configuration.');
+    }
+
+  }
+
+  JasmineExtraMatchers.$inject = ['config.files'];
+
+  module.exports = {
+    'framework:jasmine-extra-matchers': ['factory', JasmineExtraMatchers]
+  };
+
+}());
